7789b0a89749649b78f5815344f324e1f67500a7
Zhen Yu Yong <fuz3zy@gmail.com>
2020-06-19 19:04:14 +0800

    combined storage layer with service provider

--- src/storage-layer.js
+++ src/storage-layer.js
@@ -1,32 +1,32 @@
-import Torus from "@toruslabs/torus.js";
+import BN from "bn.js";
+import { keccak256 } from "web3-utils";
 
-// import BN from "bn.js";
 // import { encrypt } from "eccrypto";
 // import { decrypt, encrypt, generatePrivate, getPublic } from "eccrypto";
 // import { ec as EC } from "elliptic";
 import { post } from "./httpHelpers";
 
 class TorusStorageLayer {
-  constructor({ enableLogging = false, hostUrl = "https://metadata.tor.us" }) {
+  constructor({ enableLogging = false, hostUrl = "https://metadata.tor.us", serviceProvider }) {
     this.enableLogging = enableLogging;
-    this.torus = new Torus();
     this.hostUrl = hostUrl;
+    this.serviceProvider = serviceProvider;
   }
 
   async getMetadata() {
-    const keyDetails = this.torus.generateMetadataParams({}, this.privKey);
+    const keyDetails = this.generateMetadataParams({}, this.privKey);
     let metadataResponse;
     try {
       metadataResponse = await post(`${this.hostUrl}/get`, keyDetails);
     } catch (error) {
       return error;
     }
-    return metadataResponse;
+    return JSON.parse(atob(metadataResponse.message));
   }
 
   async setMetadata(encryptedDetails) {
-    const serializedEncryptedDetails = Buffer.from(JSON.stringify(encryptedDetails)).toString("base64");
-    const p = this.torus.generateMetadataParams(serializedEncryptedDetails, this.peggedKey);
+    const serializedEncryptedDetails = btoa(JSON.stringify(encryptedDetails));
+    const p = this.generateMetadataParams(serializedEncryptedDetails, this.peggedKey);
     let response;
     try {
       response = await post(`${this.hostUrl}/set`, p);
@@ -34,6 +34,20 @@ class TorusStorageLayer {
       return err;
     }
     return response;
+  }
+
+  generateMetadataParams(message) {
+    const setData = {
+      data: message,
+      timestamp: new BN(Date.now()).toString(16),
+    };
+    const sig = this.serviceProvider.sign(keccak256(JSON.stringify(setData)).slice(2));
+    return {
+      pub_key_X: this.serviceProvider.retrievePubKey().getX().toString("hex"),
+      pub_key_Y: this.serviceProvider.retrievePubKey().getY().toString("hex"),
+      set_data: setData,
+      signature: sig,
+    };
   }
 }
 
