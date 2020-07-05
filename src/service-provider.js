53370ef24e32e53c541972764d95e5b37ae74011
Zhen Yu Yong <fuz3zy@gmail.com>
2020-07-05 15:51:21 +0800

    remove torus.js dep

--- src/service-provider.js
+++ src/service-provider.js
@@ -1,4 +1,3 @@
-const Torus = require("@toruslabs/torus.js");
 const { BN } = require("./types.js");
 const { decrypt, encrypt } = require("./utils");
 const decryptUtils = decrypt;
@@ -12,7 +11,6 @@ class TorusServiceProvider {
   constructor({ enableLogging = false, postboxKey = "d573b6c7d8fe4ec7cbad052189d4a8415b44d8b87af024872f38db3c694d306d" } = {}) {
     this.ec = new EC("secp256k1");
     this.enableLogging = enableLogging;
-    this.torus = new Torus();
     this.postboxKey = new BN(postboxKey, 16);
     this.ecPostboxKey = this.ec.keyFromPrivate(this.postboxKey.toString("hex", 64));
   }
