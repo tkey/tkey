38718539a78fa35b5dc22023916f5801024d8847
rathishubham7 <shubhamrathi@icloud.com>
2020-09-03 12:49:24 +0800

    Updated examples with better error handling

--- examples/vue-app/src/App.vue
+++ examples/vue-app/src/App.vue
@@ -3,11 +3,7 @@
     <div>
       <span :style="{ marginRight: '20px' }">verifier:</span>
       <select v-model="selectedVerifier">
-        <option
-          :key="login"
-          v-for="login in Object.keys(verifierMap)"
-          :value="login"
-        >{{ verifierMap[login].name }}</option>
+        <option :key="login" v-for="login in Object.keys(verifierMap)" :value="login">{{ verifierMap[login].name }}</option>
       </select>
     </div>
     <div :style="{ marginTop: '20px' }" v-if="selectedVerifier === 'passwordless'">
@@ -64,7 +60,7 @@ export default {
           name: "Google",
           typeOfLogin: "google",
           clientId: "221898609709-obfn3p63741l5333093430j3qeiinaa8.apps.googleusercontent.com",
-          verifier: "google-lrc",
+          verifier: "google-lrc"
         },
         [FACEBOOK]: { name: "Facebook", typeOfLogin: "facebook", clientId: "617201755556395", verifier: "facebook-lrc" },
         [REDDIT]: { name: "Reddit", typeOfLogin: "reddit", clientId: "YNsv1YtA_o66fA", verifier: "torus-reddit-test" },
@@ -74,13 +70,13 @@ export default {
           name: "Email Password",
           typeOfLogin: "email_password",
           clientId: "sqKRBVSdwa4WLkaq419U7Bamlh5vK1H7",
-          verifier: "torus-auth0-email-password",
+          verifier: "torus-auth0-email-password"
         },
         [PASSWORDLESS]: {
           name: "Passwordless",
           typeOfLogin: "passwordless",
           clientId: "P7PJuBCXIHP41lcyty0NEb7Lgf7Zme8Q",
-          verifier: "torus-auth0-passwordless",
+          verifier: "torus-auth0-passwordless"
         },
         [APPLE]: { name: "Apple", typeOfLogin: "apple", clientId: "m1Q0gvDfOyZsJCZ3cucSQEe9XMvl9d9L", verifier: "torus-auth0-apple-lrc" },
         [GITHUB]: { name: "Github", typeOfLogin: "github", clientId: "PC2a4tfNRvXbT48t89J5am0oFM21Nxff", verifier: "torus-auth0-github-lrc" },
@@ -92,15 +88,15 @@ export default {
           name: "Hosted Email Passwordless",
           typeOfLogin: "jwt",
           clientId: "P7PJuBCXIHP41lcyty0NEb7Lgf7Zme8Q",
-          verifier: "torus-auth0-passwordless",
+          verifier: "torus-auth0-passwordless"
         },
         [HOSTED_SMS_PASSWORDLESS]: {
           name: "Hosted SMS Passwordless",
           typeOfLogin: "jwt",
           clientId: "nSYBFalV2b1MSg5b2raWqHl63tfH3KQa",
-          verifier: "torus-auth0-sms-passwordless",
-        },
-      },
+          verifier: "torus-auth0-sms-passwordless"
+        }
+      }
     };
   },
   computed: {
@@ -115,11 +111,15 @@ export default {
         [LINKEDIN]: { domain: AUTH_DOMAIN },
         [TWITTER]: { domain: AUTH_DOMAIN },
         [WEIBO]: { domain: AUTH_DOMAIN },
-        [LINE]: { domain: AUTH_DOMAIN },
+        [LINE]: { domain: AUTH_DOMAIN }
       };
-    },
+    }
   },
   methods: {
+    passwordValidation(v) {
+      return v.length >= 10
+      // return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\dA-Za-z]).\S{10,}$/.test(v)
+    },
     async login() {
       try {
         if (!this.torusdirectsdk) return;
@@ -129,45 +129,64 @@ export default {
           typeOfLogin,
           verifier,
           clientId,
-          jwtParams,
+          jwtParams
         });
 
-        // AGGREGATE LOGIN
-        // const loginDetails = await torusdirectsdk.triggerAggregateLogin({
-        //   aggregateVerifierType: "single_id_verifier",
-        //   verifierIdentifier: "google-google",
-        //   subVerifierDetailsArray: [
-        //     {
-        //       clientId: "238941746713-qqe4a7rduuk256d8oi5l0q34qtu9gpfg.apps.googleusercontent.com",
-        //       typeOfLogin: "google",
-        //       verifier: "google-shubs",
-        //     },
-        //   ],
-        // });
-
-        // AGGREGATE LOGIN - AUTH0 (Not working - Sample only)
-        // const loginDetails = await torusdirectsdk.triggerAggregateLogin({
-        //   aggregateVerifierType: "single_id_verifier",
-        //   verifierIdentifier: "google-auth0-gooddollar",
-        //   subVerifierDetailsArray: [
-        //     {
-        //       clientId: config.auth0ClientId,
-        //       typeOfLogin: "email_password",
-        //       verifier: "auth0",
-        //       jwtParams: { domain: config.auth0Domain },
-        //     },
-        //   ],
-        // });
-        
         const initializedDetails = await this.tbsdk.initialize();
         console.log(initializedDetails);
-        if (initializedDetails.requiredShares > 0) {
-          await this.tbsdk.modules.webStorage.inputShareFromWebStorage();
+
+        let shareDesc = Object.assign({}, initializedDetails.shareDescriptions);
+        Object.keys(shareDesc).map(el => {
+          shareDesc[el] = shareDesc[el].map(jl => {
+            return JSON.parse(jl);
+          });
+        });
+
+        // Check different types of shares from metadata. This helps in making UI decisions (About what kind of shares to ask from users)
+        // Sort the share descriptions with priority order
+        let priorityOrder = [
+          "webStorage",
+          "securityQuestions",
+        ];
+
+        let tempSD = Object.values(shareDesc)
+        .flatMap(x => x)
+        .sort((a, b) => {
+          return (
+            priorityOrder.indexOf(a.module) - priorityOrder.indexOf(b.module)
+          );
+        });
+
+
+        let requiredShares = initializedDetails.requiredShares;
+        while (requiredShares > 0) {
+          let currentPriority = tempSD.shift();
+          if (currentPriority.module === "webStorage") {
+            try {
+              await this.tbsdk.modules.webStorage.inputShareFromWebStorage();
+              requiredShares--;
+            } catch (err) {
+              console.log("Couldn't find on device share");
+            }
+          } else if (currentPriority.module === "securityQuestions") {
+            // default to password for now
+            throw "Password required";
+          }
+
+          if (tempSD.length === 0 && requiredShares > 0) {
+            throw "new key assign required";
+          }
         }
+
+        // if (initializedDetails.requiredShares > 0) {
+        //   await this.tbsdk.modules.webStorage.inputShareFromWebStorage();
+        // }
+
         const key = await this.tbsdk.reconstructKey();
         // await this.tbsdk.initializeNewKey(undefined, true)
-        console.log(key.toString('hex'));
+        console.log(key.toString("hex"));
         this.console(key);
+
         // this.console(initializedDetails);
       } catch (error) {
         console.error(error, "caught");
@@ -175,6 +194,10 @@ export default {
     },
     async generateNewShareWithSecurityQuestions() {
       try {
+        if(!this.passwordValidation(this.answer)){
+          this.console("Minimum length 10 characters")
+          throw "Minimum length 10 characters"
+        } 
         await this.tbsdk.modules.securityQuestions.generateNewShareWithSecurityQuestions(this.answer, "whats your password?");
         this.console("succeeded generateNewShareWithSecurityQuestions");
         console.log(this.tbsdk.getKeyDetails());
@@ -182,17 +205,21 @@ export default {
         console.error(error, "caught");
       }
     },
-    async reconstructKey(){
+    async reconstructKey() {
       try {
-        let key = await this.tbsdk.reconstructKey()
-        this.console("private key, " + key.toString('hex'));
-        console.log(key.toString('hex'), this.tbsdk.getKeyDetails());
+        let key = await this.tbsdk.reconstructKey();
+        this.console("private key, " + key.toString("hex"));
+        console.log(key.toString("hex"), this.tbsdk.getKeyDetails());
       } catch (error) {
         console.error(error, "caught");
       }
     },
     async inputShareFromSecurityQuestions() {
       try {
+        if(!this.passwordValidation(this.answer)){
+          this.console("Minimum length 10 characters")
+          throw "Minimum length 10 characters"
+        } 
         await this.tbsdk.modules.securityQuestions.inputShareFromSecurityQuestions(this.answer, "whats your password?");
         this.console("succeded inputShareFromSecurityQuestions");
         console.log(this.tbsdk.getKeyDetails());
@@ -210,6 +237,7 @@ export default {
     },
     async initializeNewKey() {
       try {
+        await this.login();
         const res = await this.tbsdk.initializeNewKey({ initializeModules: false });
         this.console(res);
       } catch (error) {
@@ -218,7 +246,7 @@ export default {
     },
     console(text) {
       document.querySelector("#console>p").innerHTML = typeof text === "object" ? JSON.stringify(text) : text;
-    },
+    }
   },
   async mounted() {
     try {
@@ -226,7 +254,7 @@ export default {
         baseUrl: `${location.origin}/serviceworker`,
         enableLogging: true,
         proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
-        network: "testnet", // details for test net
+        network: "testnet" // details for test net
       };
       const webStorageModule = new WebStorageModule();
       const securityQuestionsModule = new SecurityQuestionsModule();
@@ -235,7 +263,7 @@ export default {
       const tbsdk = new ThresholdKey({
         serviceProvider,
         storageLayer,
-        modules: { webStorage: webStorageModule, securityQuestions: securityQuestionsModule },
+        modules: { webStorage: webStorageModule, securityQuestions: securityQuestionsModule }
       });
       const torusdirectsdk = tbsdk.serviceProvider;
       // { enableLogging = false, modules = {}, serviceProvider, storageLayer, directParams }
@@ -245,7 +273,7 @@ export default {
     } catch (error) {
       console.error(error, "mounted caught");
     }
-  },
+  }
 };
 </script>
 
@@ -277,5 +305,8 @@ export default {
 #console > p {
   margin: 0.5em;
   word-wrap: break-word;
+}
+button {
+  margin-right: 10px;
 }
 </style>
