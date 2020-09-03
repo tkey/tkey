6a5e3ad51205dfe96c9b73e7771cd4e62fbfda2f
rathishubham7 <shubhamrathi@icloud.com>
2020-09-04 00:59:15 +0800

    Separated functions in example

--- examples/vue-app/src/App.vue
+++ examples/vue-app/src/App.vue
@@ -13,12 +13,13 @@
       <input v-model="answer" placeholder="enter your answer" />
     </div>
     <div :style="{ marginTop: '20px' }">
-      <button @click="login">Login with Torus</button>
-      <button @click="generateNewShareWithSecurityQuestions">generateNewShareWithSecurityQuestions</button>
-      <button @click="inputShareFromSecurityQuestions">inputShareFromSecurityQuestions</button>
-      <button @click="generateNewShare">generateNewShare</button>
-      <button @click="initializeNewKey">initializeNewKey</button>
-      <button @click="reconstructKey">reconstructKey</button>
+      <button @click="triggerLogin(true)">Login with Torus and initialize tkey</button>
+      <button @click="generateNewShareWithSecurityQuestions">Create a new password</button>
+      <button @click="inputShareFromSecurityQuestions">Add password</button>
+      <!-- <button @click="generateNewShare">generateNewShare</button> -->
+      <button @click="initializeNewKey">Create new tkey</button>
+      <button @click="reconstructKey">Reconstuct tkey</button>
+      <button @click="getKeyDetails">Get tkey details</button>
     </div>
     <div id="console">
       <p></p>
@@ -116,24 +117,19 @@ export default {
     }
   },
   methods: {
+    getKeyDetails(){
+      console.log(this.tbsdk.getKeyDetails());
+      return this.tbsdk.getKeyDetails()
+    },
     passwordValidation(v) {
       return v.length >= 10
       // return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\dA-Za-z]).\S{10,}$/.test(v)
     },
-    async login() {
-      try {
-        if (!this.torusdirectsdk) return;
-        const jwtParams = this.loginToConnectionMap[this.selectedVerifier] || {};
-        const { typeOfLogin, clientId, verifier } = this.verifierMap[this.selectedVerifier];
-        await this.torusdirectsdk.triggerLogin({
-          typeOfLogin,
-          verifier,
-          clientId,
-          jwtParams
-        });
-
+    async initializeAndReconstruct(){
+      try{
         const initializedDetails = await this.tbsdk.initialize();
         console.log(initializedDetails);
+        // console.log(initializedDetails.shareDescriptions[2].length)
 
         let shareDesc = Object.assign({}, initializedDetails.shareDescriptions);
         Object.keys(shareDesc).map(el => {
@@ -157,9 +153,12 @@ export default {
           );
         });
 
+        if(tempSD.length === 0 && requiredShares > 0) {
+          throw new Error("No share descriptions available. New key assign might be required or contact support")
+        }
 
         let requiredShares = initializedDetails.requiredShares;
-        while (requiredShares > 0) {
+        while (requiredShares > 0 && tempSD.length > 0) {
           let currentPriority = tempSD.shift();
           if (currentPriority.module === "webStorage") {
             try {
@@ -188,6 +187,23 @@ export default {
         this.console(key);
 
         // this.console(initializedDetails);
+      } catch (error) {
+        console.error(error, "caught");
+      }
+    },
+    async triggerLogin(initTkeyFlag) {
+      try {
+        if (!this.tbsdk.serviceProvider) return;
+        const jwtParams = this.loginToConnectionMap[this.selectedVerifier] || {};
+        const { typeOfLogin, clientId, verifier } = this.verifierMap[this.selectedVerifier];
+        await this.tbsdk.serviceProvider.triggerLogin({
+          typeOfLogin,
+          verifier,
+          clientId,
+          jwtParams
+        });
+
+        if(initTkeyFlag) this.initializeAndReconstruct()
       } catch (error) {
         console.error(error, "caught");
       }
@@ -230,6 +246,7 @@ export default {
     async generateNewShare() {
       try {
         const res = await this.tbsdk.generateNewShare();
+        console.log(res)
         this.console(res);
       } catch (error) {
         console.error(error, "caught");
@@ -237,19 +254,19 @@ export default {
     },
     async initializeNewKey() {
       try {
-        await this.login();
-        const res = await this.tbsdk.initializeNewKey({ initializeModules: false });
+        await this.initTkey()
+        await this.triggerLogin(false);
+        const res = await this.tbsdk.initializeNewKey({ initializeModules: true });
         this.console(res);
+        console.log("new tkey", res.toString('hex'))
       } catch (error) {
         console.error(error, "caught");
       }
     },
     console(text) {
       document.querySelector("#console>p").innerHTML = typeof text === "object" ? JSON.stringify(text) : text;
-    }
-  },
-  async mounted() {
-    try {
+    },
+    async initTkey(){
       const directParams = {
         baseUrl: `${location.origin}/serviceworker`,
         enableLogging: true,
@@ -265,11 +282,16 @@ export default {
         storageLayer,
         modules: { webStorage: webStorageModule, securityQuestions: securityQuestionsModule }
       });
-      const torusdirectsdk = tbsdk.serviceProvider;
+      // const torusdirectsdk = tbsdk.serviceProvider;
       // { enableLogging = false, modules = {}, serviceProvider, storageLayer, directParams }
-      await torusdirectsdk.init({ skipSw: false });
+      await tbsdk.serviceProvider.init({ skipSw: false });
       this.tbsdk = tbsdk;
-      this.torusdirectsdk = torusdirectsdk;
+      // this.torusdirectsdk = torusdirectsdk;
+    }
+  },
+  async mounted() {
+    try {
+      await this.initTkey()
     } catch (error) {
       console.error(error, "mounted caught");
     }
@@ -307,6 +329,6 @@ export default {
   word-wrap: break-word;
 }
 button {
-  margin-right: 10px;
+  margin: 0 10px 10px 0;
 }
 </style>
