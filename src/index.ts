1f9bf34bd60d2d3cd59f75f853a46378019ccdca
Zhen Yu Yong <fuz3zy@gmail.com>
2020-09-24 16:22:11 +0800

    added check for priv key in reconstruct

--- src/index.ts
+++ src/index.ts
@@ -213,6 +213,11 @@ class ThresholdKey implements ITKey {
       shareIndexArr.push(this.shares[pubPolyID][polyShares[i]].share.shareIndex);
     }
     const privKey = lagrangeInterpolation(shareArr, shareIndexArr);
+    // check that priv key regenerated is correct
+    const reconstructedPubKey = getPubKeyPoint(privKey);
+    if (this.metadata.pubKey.x.cmp(reconstructedPubKey.x) !== 0) {
+      throw new Error(`reconstructed key is not pub key`);
+    }
     this.setKey(privKey);
     return this.privKey;
   }
