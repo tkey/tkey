809b8b3ebc97ab5811637cd8d393776245fab6d4
rathishubham7 <shubhamrathi@icloud.com>
2020-09-28 19:18:37 +0800

    fixed example

--- examples/vue-app/src/App.vue
+++ examples/vue-app/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <p class="font-italic"> Note: This is a testing application. Please open console for debugging. </p>
     <div>
       <span :style="{ marginRight: '20px' }">verifier:</span>
       <select v-model="selectedVerifier">
@@ -9,15 +10,20 @@
     <div :style="{ marginTop: '20px' }" v-if="selectedVerifier === 'passwordless'">
       <input type="email" v-model="loginHint" placeholder="Enter your email" />
     </div>
-    
+
+    <div :style="{ marginTop: '20px' }">
+      <input type="checkbox" id="mocked" v-model="mocked" />
+      <label for="mocked">Mocked Login</label>
+    </div>
+
     <div :style="{ marginTop: '20px' }">
       <h4>Login and resets</h4>
-      <button @click="triggerLogin(true)">Login with Torus and initialize tkey</button>
+      <button @click="triggerLogin">Login with Torus and initialize tkey</button>
       <button @click="initializeNewKey">Create new tkey</button>
       <button @click="reconstructKey">Reconstuct tkey</button>
       <button @click="getKeyDetails">Get tkey details</button>
       <button @click="getSDKObject">Get tkey object</button>
-      <br>
+      <br />
       <h4>Add and removing shares</h4>
       <div :style="{ margin: '20px' }">
         <input v-model="answer" placeholder="enter your answer" />
@@ -25,12 +31,12 @@
       <button @click="generateNewShareWithSecurityQuestions">Create a new password</button>
       <button @click="inputShareFromSecurityQuestions">Add password</button>
       <!-- <button @click="generateNewShare">generateNewShare</button> -->
-      <br>
+      <br />
       <h4>Share Transer</h4>
-      <button @click="checkShareRequests"> Check share requests </button>
-      <button @click="requestShare"> Request Share </button>
-      <button @click="approveShareRequest"> Approve request </button>
-      <button @click="resetShareRequests"> Reset share request store </button>
+      <button @click="checkShareRequests">Check share requests</button>
+      <button @click="requestShare">Request Share</button>
+      <button @click="approveShareRequest">Approve request</button>
+      <button @click="resetShareRequests">Reset share request store</button>
     </div>
     <div id="console">
       <p></p>
@@ -39,7 +45,14 @@
 </template>
 
 <script>
-import ThresholdKey, { WebStorageModule, SecurityQuestionsModule, TorusStorageLayer, ServiceProviderBase, ShareTransferModule } from "@tkey/core";
+import ThresholdKey, {
+  WebStorageModule,
+  SecurityQuestionsModule,
+  TorusStorageLayer,
+  TorusServiceProvider,
+  ServiceProviderBase,
+  ShareTransferModule
+} from "@tkey/core";
 // import ServiceProviderBase from '../../../src/serviceProvider/ServiceProviderBase';
 
 const GOOGLE = "google";
@@ -67,6 +80,7 @@ export default {
       torusdirectsdk: undefined,
       selectedVerifier: "google",
       loginHint: "",
+      mocked: true,
       answer: "",
       verifierMap: {
         [GOOGLE]: {
@@ -129,30 +143,26 @@ export default {
     }
   },
   methods: {
-    getKeyDetails(){
+    getKeyDetails() {
       console.log(this.tbsdk.getKeyDetails());
-      return this.tbsdk.getKeyDetails()
+      return this.tbsdk.getKeyDetails();
     },
-    getLatestPolynomialDetails(){
-      const metadata = this.tbSDK.getMetadata()
-      let latestPolynomial = metadata.getLatestPublicPolynomial()
-      let latestPolynomialId = latestPolynomial.getPolynomialID()
-      let indexes = metadata.getShareIndexesForPolynomial(latestPolynomialId)
-      console.log(latestPolynomial, latestPolynomialId, indexes)
+    getLatestPolynomialDetails() {
+      const metadata = this.tbSDK.getMetadata();
+      let latestPolynomial = metadata.getLatestPublicPolynomial();
+      let latestPolynomialId = latestPolynomial.getPolynomialID();
+      let indexes = metadata.getShareIndexesForPolynomial(latestPolynomialId);
+      console.log(latestPolynomial, latestPolynomialId, indexes);
     },
-    getSDKObject(){
-      // let latestPolynomial = metadata.getLatestPublicPolynomial()
-      // let latestPolynomialId = latestPolynomial.getPolynomialID()
-      // let indexes = metadata.getShareIndexesForPolynomial(latestPolynomialId)
-      // console.log(latestPolynomial, latestPolynomialId, indexes)
-      console.log(this.tbsdk)
+    getSDKObject() {
+      console.log(this.tbsdk);
     },
     passwordValidation(v) {
-      return v.length >= 10
+      return v.length >= 10;
       // return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\dA-Za-z]).\S{10,}$/.test(v)
     },
-    async initializeAndReconstruct(){
-      try{
+    async initializeAndReconstruct() {
+      try {
         const initializedDetails = await this.tbsdk.initialize();
         console.log(initializedDetails);
         // console.log(initializedDetails.shareDescriptions[2].length)
@@ -166,21 +176,16 @@ export default {
 
         // Check different types of shares from metadata. This helps in making UI decisions (About what kind of shares to ask from users)
         // Sort the share descriptions with priority order
-        let priorityOrder = [
-          "webStorage",
-          "securityQuestions",
-        ];
+        let priorityOrder = ["webStorage", "securityQuestions"];
 
         let tempSD = Object.values(shareDesc)
-        .flatMap(x => x)
-        .sort((a, b) => {
-          return (
-            priorityOrder.indexOf(a.module) - priorityOrder.indexOf(b.module)
-          );
-        });
+          .flatMap(x => x)
+          .sort((a, b) => {
+            return priorityOrder.indexOf(a.module) - priorityOrder.indexOf(b.module);
+          });
 
-        if(tempSD.length === 0 && requiredShares > 0) {
-          throw new Error("No share descriptions available. New key assign might be required or contact support")
+        if (tempSD.length === 0 && requiredShares > 0) {
+          throw new Error("No share descriptions available. New key assign might be required or contact support");
         }
 
         let requiredShares = initializedDetails.requiredShares;
@@ -213,29 +218,33 @@ export default {
         console.error(error, "caught");
       }
     },
-    async triggerLogin(initTkeyFlag) {
+    async triggerLogin() {
       try {
-        if (!this.tbsdk.serviceProvider) return;
-        // const jwtParams = this.loginToConnectionMap[this.selectedVerifier] || {};
-        // const { typeOfLogin, clientId, verifier } = this.verifierMap[this.selectedVerifier];
-        // await this.tbsdk.serviceProvider.triggerLogin({
-        //   typeOfLogin,
-        //   verifier,
-        //   clientId,
-        //   jwtParams
-        // });
+        await this.initTkey()
 
-        if(initTkeyFlag) this.initializeAndReconstruct()
+        // console.log(this.tbsdk, this.tbsdk.serviceProvider, this.tbsdk.serviceProvider.__proto__ )
+        if (!this.tbsdk.serviceProvider) return;
+        if (!this.mocked) {
+          const jwtParams = this.loginToConnectionMap[this.selectedVerifier] || {};
+          const { typeOfLogin, clientId, verifier } = this.verifierMap[this.selectedVerifier];
+          await this.tbsdk.serviceProvider.triggerLogin({
+            typeOfLogin,
+            verifier,
+            clientId,
+            jwtParams
+          });
+        }
+        await this.initializeAndReconstruct();
       } catch (error) {
         console.error(error, "caught");
       }
     },
     async generateNewShareWithSecurityQuestions() {
       try {
-        if(!this.passwordValidation(this.answer)){
-          this.console("Minimum length 10 characters")
-          throw "Minimum length 10 characters"
-        } 
+        if (!this.passwordValidation(this.answer)) {
+          this.console("Minimum length 10 characters");
+          throw "Minimum length 10 characters";
+        }
         await this.tbsdk.modules.securityQuestions.generateNewShareWithSecurityQuestions(this.answer, "whats your password?");
         this.console("succeeded generateNewShareWithSecurityQuestions");
         console.log(this.tbsdk.getKeyDetails());
@@ -243,50 +252,50 @@ export default {
         console.error(error, "caught");
       }
     },
-    async checkShareRequests(){
-      try{
-        const result = await this.tbsdk.modules.shareTransfer.getShareTransferStore()
-        const requests = await this.tbsdk.modules.shareTransfer.lookForRequests()
-        console.log(result, requests)
-      }catch(err){
-        console.log(err)
+    async checkShareRequests() {
+      try {
+        const result = await this.tbsdk.modules.shareTransfer.getShareTransferStore();
+        const requests = await this.tbsdk.modules.shareTransfer.lookForRequests();
+        console.log(result, requests);
+      } catch (err) {
+        console.log(err);
       }
     },
-    async resetShareRequests(){
-      try{
-        await this.tbsdk.modules.shareTransfer.resetShareTransferStore()
-        this.console("Reset share transfer store successfully")
-      }catch(err){
-        console.log(err)
+    async resetShareRequests() {
+      try {
+        await this.tbsdk.modules.shareTransfer.resetShareTransferStore();
+        this.console("Reset share transfer store successfully");
+      } catch (err) {
+        console.log(err);
       }
     },
-    async approveShareRequest(){
-      try{
-        const result = await this.tbsdk.modules.shareTransfer.getShareTransferStore()
-        const requests = await this.tbsdk.modules.shareTransfer.lookForRequests()
-        let shareToShare
-        try{
-          shareToShare = await this.tbsdk.modules.webStorage.getDeviceShare()
-        }catch(err){
-          console.error("No on device share found. Generating a new share")
-          const newShare = await this.tbsdk.generateNewShare()
-          shareToShare = newShare.newShareStores[newShare.newShareIndex.toString("hex")]
+    async approveShareRequest() {
+      try {
+        const result = await this.tbsdk.modules.shareTransfer.getShareTransferStore();
+        const requests = await this.tbsdk.modules.shareTransfer.lookForRequests();
+        let shareToShare;
+        try {
+          shareToShare = await this.tbsdk.modules.webStorage.getDeviceShare();
+        } catch (err) {
+          console.error("No on device share found. Generating a new share");
+          const newShare = await this.tbsdk.generateNewShare();
+          shareToShare = newShare.newShareStores[newShare.newShareIndex.toString("hex")];
         }
-        console.log(result, requests, this.tbsdk)
+        console.log(result, requests, this.tbsdk);
 
-        await this.tbsdk.modules.shareTransfer.approveRequest(requests[0], shareToShare)
+        await this.tbsdk.modules.shareTransfer.approveRequest(requests[0], shareToShare);
         // await this.tbsdk.modules.shareTransfer.deleteShareTransferStore(requests[0]) // delete old share requests
-        this.console("approved")
-      }catch(err){
-        console.error(err)
+        this.console("approved");
+      } catch (err) {
+        console.error(err);
       }
     },
-    async requestShare(){
-      try{
-        const result = await this.tbsdk.modules.shareTransfer.requestNewShare()
-        console.log(result)
-      }catch(err){
-        console.error(err)
+    async requestShare() {
+      try {
+        const result = await this.tbsdk.modules.shareTransfer.requestNewShare();
+        console.log(result);
+      } catch (err) {
+        console.error(err);
       }
     },
     async reconstructKey() {
@@ -300,10 +309,10 @@ export default {
     },
     async inputShareFromSecurityQuestions() {
       try {
-        if(!this.passwordValidation(this.answer)){
-          this.console("Minimum length 10 characters")
-          throw "Minimum length 10 characters"
-        } 
+        if (!this.passwordValidation(this.answer)) {
+          this.console("Minimum length 10 characters");
+          throw "Minimum length 10 characters";
+        }
         await this.tbsdk.modules.securityQuestions.inputShareFromSecurityQuestions(this.answer, "whats your password?");
         this.console("succeded inputShareFromSecurityQuestions");
         console.log(this.tbsdk.getKeyDetails());
@@ -314,7 +323,7 @@ export default {
     async generateNewShare() {
       try {
         const res = await this.tbsdk.generateNewShare();
-        console.log(res)
+        console.log(res);
         this.console(res);
       } catch (error) {
         console.error(error, "caught");
@@ -322,46 +331,50 @@ export default {
     },
     async initializeNewKey() {
       try {
-        await this.initTkey()
-        await this.triggerLogin(false);
+        await this.initTkey();
+        if(!this.mocked) await this.triggerLogin();
         const res = await this.tbsdk.initializeNewKey({ initializeModules: true });
         this.console(res);
-        console.log("new tkey", res)
+        console.log("new tkey", res);
       } catch (error) {
         console.error(error, "caught");
       }
     },
-    console(text) {
-      document.querySelector("#console>p").innerHTML = typeof text === "object" ? JSON.stringify(text) : text;
-    },
-    async initTkey(){
-      // const directParams = {
-      //   baseUrl: `${location.origin}/serviceworker`,
-      //   enableLogging: true,
-      //   proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
-      //   network: "testnet" // details for test net
-      // };
+    async initTkey() {
+      const directParams = {
+        baseUrl: `${location.origin}/serviceworker`,
+        enableLogging: true,
+        proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
+        network: "testnet" // details for test net
+      };
       const webStorageModule = new WebStorageModule();
       const securityQuestionsModule = new SecurityQuestionsModule();
-      const shareTransferModule = new ShareTransferModule()
-      // const serviceProvider = new TorusServiceProvider({ directParams });
-      const serviceProviderBase = new ServiceProviderBase({postboxKey:"f1f02ee186749cfe1ef8f957fc3d7a5b7128f979bacc10ab3b2a811d4f990852"})
-      const storageLayer = new TorusStorageLayer({ hostUrl: "https://metadata.tor.us", serviceProvider: serviceProviderBase });
+      const shareTransferModule = new ShareTransferModule();
+
+      let serviceProvider;
+      if (this.mocked) {
+        serviceProvider = new ServiceProviderBase({ postboxKey: "f1f02ee186749cfe1ef8f957fc3d7a5b7128f979bacc10ab3b2a811d4f990852" });
+      } else {
+        serviceProvider = new TorusServiceProvider({ directParams });
+      }
+      const storageLayer = new TorusStorageLayer({ hostUrl: "https://metadata.tor.us", serviceProvider: serviceProvider });
       const tbsdk = new ThresholdKey({
-        serviceProvider: serviceProviderBase,
+        serviceProvider: serviceProvider,
         storageLayer,
         modules: { webStorage: webStorageModule, securityQuestions: securityQuestionsModule, shareTransfer: shareTransferModule }
       });
-      const torusdirectsdk = tbsdk.serviceProvider;
-      // { enableLogging = false, modules = {}, serviceProvider, storageLayer, directParams }
-      // await tbsdk.serviceProvider.init({ skipSw: false });
       this.tbsdk = tbsdk;
-      this.torusdirectsdk = torusdirectsdk;
+      this.torusdirectsdk = tbsdk.serviceProvider;
+
+      if(!this.mocked) await tbsdk.serviceProvider.init({ skipSw: false });
+    },
+    console(text) {
+      document.querySelector("#console>p").innerHTML = typeof text === "object" ? JSON.stringify(text) : text;
     }
   },
   async mounted() {
     try {
-      await this.initTkey()
+      await this.initTkey();
     } catch (error) {
       console.error(error, "mounted caught");
     }
@@ -397,6 +410,9 @@ export default {
 #console > p {
   margin: 0.5em;
   word-wrap: break-word;
+}
+#font-italic{
+  font-style: italic;
 }
 button {
   margin: 0 10px 10px 0;
