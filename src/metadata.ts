c68972fd5202f0ce8b1e7615e60ceea649c1feb0
Zhen Yu Yong <fuz3zy@gmail.com>
2020-09-29 20:24:10 +0800

    cleaned up redundent code

--- src/metadata.ts
+++ src/metadata.ts
@@ -174,47 +174,9 @@ class Metadata implements IMetadata {
       generalStore: generalStoreCopy,
       tKeyStore: this.tkeyStore,
     };
-
-    // return this;
   }
 
   static fromJSON(value: StringifiedType): Metadata {
-    // const { pubKey, polyIDList, generalStore, tkeyStore, scopedStore, shareDescriptions, publicPolynomials, publicShares } = value;
-    // const point = new Point(pubKey.x, pubKey.y);
-    // const metadata = new Metadata(point);
-    // metadata.polyIDList = polyIDList;
-    // if (generalStore) metadata.generalStore = generalStore;
-    // if (tkeyStore) metadata.tkeyStore = tkeyStore;
-    // if (scopedStore) metadata.scopedStore = scopedStore;
-    // if (shareDescriptions) metadata.shareDescriptions = shareDescriptions;
-
-    // // for publicPolynomials
-    // for (const pubPolyID in publicPolynomials) {
-    //   if (Object.prototype.hasOwnProperty.call(publicPolynomials, pubPolyID)) {
-    //     const pointCommitments = [];
-    //     publicPolynomials[pubPolyID].polynomialCommitments.forEach((commitment) => {
-    //       pointCommitments.push(new Point(commitment.x, commitment.y));
-    //     });
-    //     const publicPolynomial = new PublicPolynomial(pointCommitments);
-    //     metadata.publicPolynomials[pubPolyID] = publicPolynomial;
-    //   }
-    // }
-    // // for publicShares
-    // for (const pubPolyID in publicShares) {
-    //   if (Object.prototype.hasOwnProperty.call(publicShares, pubPolyID)) {
-    //     for (const shareIndex in publicShares[pubPolyID]) {
-    //       if (Object.prototype.hasOwnProperty.call(publicShares[pubPolyID], shareIndex)) {
-    //         const newPubShare = new PublicShare(
-    //           publicShares[pubPolyID][shareIndex].shareIndex,
-    //           new Point(publicShares[pubPolyID][shareIndex].shareCommitment.x, publicShares[pubPolyID][shareIndex].shareCommitment.y)
-    //         );
-    //         metadata.addPublicShare(pubPolyID, newPubShare);
-    //       }
-    //     }
-    //   }
-    // }
-    // return metadata;
-
     const { pubKey, polyIDList, generalStore, tkeyStore, scopedStore } = value;
     const point = Point.fromCompressedPub(pubKey);
     const metadata = new Metadata(point);
