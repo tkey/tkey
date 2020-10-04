3c97e5c9a2bc5459e134aeee1719cad0ccbe07e6
chaitanyapotti <chaitanya.potti@gmail.com>
2020-10-04 18:23:50 +0800

    remove types folder

--- types/src/webStorage/LocalStorageHelpers.d.ts
+++
@@ -1,3 +0,0 @@
-import { ShareStore } from "../base";
-export declare const storeShareOnLocalStorage: (share: ShareStore) => Promise<void>;
-export declare const getShareFromLocalStorage: (polyID: string) => Promise<ShareStore>;
