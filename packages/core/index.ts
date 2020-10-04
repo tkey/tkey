1fe4da9ec4ad95c56de29234cffdc6096a21551d
chaitanyapotti <chaitanya.potti@gmail.com>
2020-10-04 18:09:46 +0800

    share transfer

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,3 @@
 import ThresholdKey from "./src/index";
-import ShareTransferModule from "./src/shareTransfer/ShareTransferModule";
 
 export default ThresholdKey;
-
-export { ShareTransferModule };
---
+++ packages/share-transfer/index.ts
@@ -0,0 +1,4 @@
+export { default as ShareTransferModule } from "./src/ShareTransferModule";
+export { default as ShareTransferStorePointer } from "./src/ShareTransferStorePointer";
+export { default as ShareRequest } from "./src/ShareRequest";
+export { default } from "./src/ShareTransferModule";
