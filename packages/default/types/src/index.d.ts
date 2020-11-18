31bc35926f93b3c18b98ab991c598617b225c87d
rathishubham7 <shubhamrathi@icloud.com>
2020-11-18 13:14:29 +0800

    added types

--- packages/default/types/src/index.d.ts
+++ packages/default/types/src/index.d.ts
@@ -1,6 +1,7 @@
-import { TKeyArgs } from "@tkey/common-types";
+import { StringifiedType, TKeyArgs } from "@tkey/common-types";
 import TKey from "@tkey/core";
 declare class ThresholdKey extends TKey {
     constructor(args?: TKeyArgs);
+    static fromJSON(value: StringifiedType, args?: TKeyArgs): Promise<ThresholdKey>;
 }
 export default ThresholdKey;
