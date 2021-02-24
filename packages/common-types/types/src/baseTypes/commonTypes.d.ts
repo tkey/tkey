29dc45547d545c56c605808edd0cf03eb53b40ef
rathishubham7 <shubhamrathi@icloud.com>
2021-02-24 11:24:11 +0800

    updated load tests

--- packages/common-types/types/src/baseTypes/commonTypes.d.ts
+++ packages/common-types/types/src/baseTypes/commonTypes.d.ts
@@ -56,20 +56,13 @@ export interface IStorageLayer extends ISerializable {
     }): Promise<{
         message: string;
     }>;
-    setMetadataBulk<T>(params: {
+    setMetadataStream<T>(params: {
         input: T[];
         serviceProvider?: IServiceProvider;
         privKey?: BN[];
     }): Promise<{
         message: string;
-    }[]>;
-    setMetadataBulkStream<T>(params: {
-        input: T[];
-        serviceProvider?: IServiceProvider;
-        privKey?: BN[];
-    }): Promise<{
-        message: string;
-    }[]>;
+    }>;
     acquireWriteLock(params: {
         serviceProvider?: IServiceProvider;
         privKey?: BN;
--- packages/storage-layer-torus/types/src/TorusStorageLayer.d.ts
+++ packages/storage-layer-torus/types/src/TorusStorageLayer.d.ts
@@ -13,13 +13,6 @@ declare class TorusStorageLayer implements IStorageLayer {
         serviceProvider?: IServiceProvider;
         privKey?: BN;
     }): Promise<T>;
-    setMetadataBulkStream<T>(params: {
-        input: Array<T>;
-        serviceProvider?: IServiceProvider;
-        privKey?: Array<BN>;
-    }): Promise<{
-        message: string;
-    }[]>;
     /**
      * Set Metadata for a key
      * @param input data to post
@@ -32,18 +25,13 @@ declare class TorusStorageLayer implements IStorageLayer {
     }): Promise<{
         message: string;
     }>;
-    /**
-     * Set Metadata for keys
-     * @param input data to post
-     * @param privKey If not provided, it will use service provider's share for encryption
-     */
-    setMetadataBulk<T>(params: {
+    setMetadataStream<T>(params: {
         input: Array<T>;
         serviceProvider?: IServiceProvider;
         privKey?: Array<BN>;
     }): Promise<{
         message: string;
-    }[]>;
+    }>;
     generateMetadataParams(message: unknown, serviceProvider?: IServiceProvider, privKey?: BN): TorusStorageLayerAPIParams;
     acquireWriteLock(params: {
         serviceProvider?: IServiceProvider;
