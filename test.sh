3fe4f8c571865dde3cf2b7af6c1068c6710a9dea
rathishubham7 <shubhamrathi@icloud.com>
2021-02-24 11:25:27 +0800

    cleanup

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yarn run test-development --scope=@tkey/default &
-yarn run test-development --scope=@tkey/default &
-yarn run test-development --scope=@tkey/default &
-yarn run test-development --scope=@tkey/default &
-yarn run test-development --scope=@tkey/default &
