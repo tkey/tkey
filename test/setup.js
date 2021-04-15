919639b57ca3831484d800d8552e3abb67cace22
chaitanyapotti <chaitanya.potti@gmail.com>
2021-04-15 15:42:14 +0800

    update deps and clean up

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const path = require("path");
-const fetch = require("node-fetch");
-const formData = require('form-data');
-const atob = require("atob");
-const btoa = require("btoa");
 require("jsdom-global")("<!doctype html><html><body></body></html>", {
   url: "https://example.com",
 });
@@ -16,8 +12,3 @@ register({
   extensions: [".ts", ".js"],
   rootMode: "upward",
 });
-
-global.FormData = formData
-global.fetch = fetch;
-global.atob = atob;
-global.btoa = btoa;
