24e0336eafa8145dbaac2bac2ca734d6056b7def
ieow <cwlee0@gmail.com>
2023-01-31 18:23:44 +0800

    feat: function.js

--- packages/default/test/migration_tests/function.js
+++ packages/default/test/migration_tests/function.js
@@ -3,10 +3,15 @@
 import { post } from "@toruslabs/http-helpers";
 console.log("enter file");
 globalThis.js_post = async function (url, data) {
-    console.log("enter function");
-    console.log(url);
-    console.log(data);
-  //   const result = await post(url, JSON.stringify(data));
+  console.log("enter function");
+  console.log(url);
+  console.log(data);
+  try {
+    const result = await post(url, JSON.stringify(data));
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
   //   return result;
   const testdata = {
     message: "test success",
