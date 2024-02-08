4102f5077294f9e3c3bff3b5597d43b1ee30ce52
ieow <cwlee0@gmail.com>
2024-02-08 18:28:59 +0800

    feat: update lagrange function

--- packages/core/src/lagrangeInterpolatePolynomial.ts
+++ packages/core/src/lagrangeInterpolatePolynomial.ts
@@ -1,4 +1,4 @@
-import { ecCurve, generatePrivateExcludingIndexes, KeyType, Point, Polynomial, Share } from "@tkey/common-types";
+import { generatePrivateExcludingIndexes, KeyType, keyTypeToCurve, Point, Polynomial, Share } from "@tkey/common-types";
 import { generatePrivate } from "@toruslabs/eccrypto";
 import BN from "bn.js";
 import { curve, ec as EllipticCurve } from "elliptic";
@@ -7,7 +7,7 @@ import CoreError from "./errors";
 
 const generateEmptyBNArray = (length: number): BN[] => Array.from({ length }, () => new BN(0));
 
-const denominator = (i: number, innerPoints: Array<Point>) => {
+const denominator = (i: number, innerPoints: Array<Point>, ecCurve: EllipticCurve) => {
   let result = new BN(1);
   const xi = innerPoints[i].x;
   for (let j = innerPoints.length - 1; j >= 0; j -= 1) {
@@ -22,9 +22,9 @@ const denominator = (i: number, innerPoints: Array<Point>) => {
   return result;
 };
 
-const interpolationPoly = (i: number, innerPoints: Array<Point>): BN[] => {
+const interpolationPoly = (i: number, innerPoints: Array<Point>, ecCurve: EllipticCurve): BN[] => {
   let coefficients = generateEmptyBNArray(innerPoints.length);
-  const d = denominator(i, innerPoints);
+  const d = denominator(i, innerPoints, ecCurve);
   if (d.cmp(new BN(0)) === 0) {
     throw CoreError.default("Denominator for interpolationPoly is 0");
   }
@@ -60,11 +60,11 @@ const pointSort = (innerPoints: Point[]): Point[] => {
   return pointArrClone;
 };
 
-const lagrange = (unsortedPoints: Point[]) => {
+const lagrange = (unsortedPoints: Point[], ecCurve: EllipticCurve) => {
   const sortedPoints = pointSort(unsortedPoints);
   const polynomial = generateEmptyBNArray(sortedPoints.length);
   for (let i = 0; i < sortedPoints.length; i += 1) {
-    const coefficients = interpolationPoly(i, sortedPoints);
+    const coefficients = interpolationPoly(i, sortedPoints, ecCurve);
     for (let k = 0; k < sortedPoints.length; k += 1) {
       let tmp = new BN(sortedPoints[i].y);
       tmp = tmp.mul(coefficients[k]);
@@ -75,11 +75,11 @@ const lagrange = (unsortedPoints: Point[]) => {
   return new Polynomial(polynomial);
 };
 
-export function lagrangeInterpolatePolynomial(points: Array<Point>): Polynomial {
-  return lagrange(points);
+export function lagrangeInterpolatePolynomial(points: Array<Point>, ecCurve: EllipticCurve): Polynomial {
+  return lagrange(points, ecCurve);
 }
 
-export function lagrangeInterpolation(shares: BN[], nodeIndex: BN[]): BN {
+export function lagrangeInterpolation(shares: BN[], nodeIndex: BN[], ecCurve: EllipticCurve): BN {
   if (shares.length !== nodeIndex.length) {
     throw CoreError.default("shares not equal to nodeIndex length in lagrangeInterpolation");
   }
@@ -106,19 +106,19 @@ export function lagrangeInterpolation(shares: BN[], nodeIndex: BN[]): BN {
 // generateRandomPolynomial - determinisiticShares are assumed random
 export function generateRandomPolynomial(
   degree: number,
-  eCurve: EllipticCurve,
+  ecCurve: EllipticCurve,
   secret?: BN,
   deterministicShares?: Array<Share>,
   keyType?: KeyType
 ): Polynomial {
   let actualS = secret;
   if (!secret) {
-    actualS = generatePrivateExcludingIndexes([new BN(0)], eCurve);
+    actualS = generatePrivateExcludingIndexes([new BN(0)], ecCurve);
   }
   if (!deterministicShares) {
     const poly = [actualS];
     for (let i = 0; i < degree; i += 1) {
-      const share = generatePrivateExcludingIndexes(poly, eCurve);
+      const share = generatePrivateExcludingIndexes(poly, ecCurve);
       poly.push(share);
     }
     return new Polynomial(poly);
@@ -135,18 +135,19 @@ export function generateRandomPolynomial(
     points[share.shareIndex.toString("hex") as string] = new Point(share.shareIndex, share.share, keyType);
   });
   for (let i = 0; i < degree - deterministicShares.length; i += 1) {
-    let shareIndex = generatePrivateExcludingIndexes([new BN(0)], eCurve);
+    let shareIndex = generatePrivateExcludingIndexes([new BN(0)], ecCurve);
     while (points[shareIndex.toString("hex")] !== undefined) {
-      shareIndex = generatePrivateExcludingIndexes([new BN(0)], eCurve);
+      shareIndex = generatePrivateExcludingIndexes([new BN(0)], ecCurve);
     }
     points[shareIndex.toString("hex")] = new Point(shareIndex, new BN(generatePrivate()), keyType);
   }
   points["0"] = new Point(new BN(0), actualS, keyType);
-  return lagrangeInterpolatePolynomial(Object.values(points));
+  return lagrangeInterpolatePolynomial(Object.values(points), ecCurve);
 }
 
 //  2 + 3x = y | secret for index 1 is 5 >>> g^5 is the commitment | now we have g^2, g^3 and 1, |
 export function polyCommitmentEval(polyCommitments: Array<Point>, index: BN, keyType?: KeyType): Point {
+  const ecCurve = keyTypeToCurve(keyType);
   // convert to base points, this is badly written, its the only way to access the point rn zzz TODO: refactor
   const basePtPolyCommitments: Array<curve.base.BasePoint> = [];
   for (let i = 0; i < polyCommitments.length; i += 1) {
