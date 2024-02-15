4904fdaa42085c48a4c6766fd3d0cd14e8ff4b10
ieow <cwlee0@gmail.com>
2024-02-15 17:05:13 +0800

    fix: package.json storagelayer and test

--- packages/storage-layer-torus/test/test.spec.ts
+++ packages/storage-layer-torus/test/test.spec.ts
@@ -6,7 +6,8 @@ import { BN } from "bn.js";
 
 import TorusStorageLayer from "../src/TorusStorageLayer";
 
-const metadataURL = "https://node-1.node.web3auth.io/metadata";
+const metadataURL = process.env.METADATA || "https://node-1.node.web3auth.io/metadata";
+
 const keyTypes = [KeyType.secp256k1, KeyType.ed25519];
 keyTypes.forEach((keyType) => {
   describe(`TorusStorageLayer test, keyType ${keyType}`, function () {
@@ -15,9 +16,9 @@ keyTypes.forEach((keyType) => {
       const tmp = new BN(123);
       const message = tmp.toBuffer();
       const storageLayer = new TorusStorageLayer({ hostUrl: metadataURL });
-      const _ = await storageLayer.setMetadata({ input: message, privKey, keyType });
+      await storageLayer.setMetadata({ input: message, privKey, keyType });
 
-      const readResult = await storageLayer.getMetadata({ privKey, keyType });
+      const readResult = (await storageLayer.getMetadata({ privKey, keyType })) as { data: string };
       assert(Buffer.from(readResult.data).equals(Buffer.from(message)));
     });
   });
